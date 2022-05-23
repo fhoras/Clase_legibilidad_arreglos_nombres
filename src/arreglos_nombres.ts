f929245a2f245d30f294d4889b2d0ed172aa54a7
fhoras <104107218+fhoras@users.noreply.github.com>
2022-05-23 19:38:46 -0300

    Initial commit

---
+++ src/arreglos_nombres.ts
@@ -0,0 +1,18 @@
+/* Crear un arreglo de las posiciones que desee
+el usuario y llenarlo con nombres de personas */
+let cant = document.getElementById("cantidad");
+
+function arreglo_nombres(cantidad: number) {
+  let nombre: string = "";
+  let nombres: string[] = [];
+  for (let i = 0; i < cantidad; i++) {
+    nombre = prompt("Ingrese nombre");
+    nombres[i] = nombre;
+  }
+  console.log(nombres);
+}
+
+btnDatos1.addEventListener("click", () => {
+  let cantidad: number = cant.value;
+  console.log(arreglo_nombres(cantidad));
+});
